76e37c3e02cf3e9143026a598bc7b7bd4eed5832
Agakitsune <raphael.turpin@epitech.eu>
2022-06-21 20:51:17 +0400

    Added Action

---
+++ src/GUI/Action.cpp
@@ -0,0 +1,10 @@
+#include "qt20/GUI/Action"
+
+namespace qt20 {
+    namespace gui {
+        Action::Action(const std::string &text) : QAction(QString(text.c_str)) {}
+        Action::Action() : QAction() {}
+
+    } // namespace gui
+
+} // namespace qt20
